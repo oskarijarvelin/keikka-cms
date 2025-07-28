9c5be951ee5b5f969f7ba1fd49efd9c5bae95f91
Oskari Järvelin <contact@oskarijarvelin.fi>
2025-07-28 17:33:15 +0300

    Clarify meta tag usage

--- src/createEmotionCache.js
+++ src/createEmotionCache.js
@@ -2,9 +2,11 @@ import createCache from '@emotion/cache';
 
 const isBrowser = typeof document !== 'undefined';
 
-// On the client side, Create a meta tag at the top of the <head> and set it as insertionPoint.
-// This assures that MUI styles are loaded first.
-// It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
+// On the client side, this function looks for an existing meta tag named
+// `emotion-insertion-point` at the top of the <head> and uses it as the
+// insertion point. This assures that MUI styles are loaded first and allows
+// developers to easily override them with other styling solutions, like CSS
+// modules.
 export default function createEmotionCache() {
   let insertionPoint;
 
